2d9b03d5569a1dabb9de52bd996efeef542f40d5
Abhigyan-Tripathi001 <abhigyantripathi42@gmail.com>
2025-09-09 01:00:07 +0530

    fourth commit

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -17,7 +17,7 @@ export async function POST(request) {
 
         // Calculate amount using items
         const amount = await items.reduce(async (acc, item) => {
-            const product = await Product.findById(items.product);
+            const product = await Product.findById(item.product);
             return acc + product.offerPrice * item.quantity;
 
         },0)
