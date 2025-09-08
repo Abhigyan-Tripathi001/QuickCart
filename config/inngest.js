1c0377f8d4494e86a90f3a96e8038204632f18de
Abhigyan-Tripathi001 <abhigyantripathi42@gmail.com>
2025-09-09 00:31:15 +0530

    second commit

--- config/inngest.js
+++ config/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import connectDB from "./db";
 import User from "@/models/User";
+import Order from "@/models/Order";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "quickcart-next" });
@@ -10,7 +11,7 @@ export const syncUserCreation = inngest.createFunction(
     {
         id:'sync-user-from-clerk'
     },
-    { event: 'clerk/user.created' }
+    { event: 'clerk/user.created' },
     async({event}) => {
         const {id, first_name, last_name, email_addresses, image_url} = event.data
         const userData = {
@@ -56,5 +57,34 @@ export const syncUserDeletion = inngest.createFunction(
 
         await connectDB()
         await User.findByIdAndDelete(id)
+    }
+)
+
+// Inngest Function to create user's order in database
+export const createUserOrder = inngest.createFunction(
+    {
+        id: 'create-user-order',
+        batchEvents: {
+            maxSize: 25,
+            timeout: '5s'
+        }
+    },
+    {event: 'order-created'},
+    async ({events})=>{
+
+        const orders = events.map((event)=>{
+            return {
+                userId: event.data.userId,
+                items: event.data.items,
+                amount: event.data.amount,
+                address: event.data.address,
+                date: event.data.date
+            }
+        })
+
+        await connectDB()
+        await Order.insertMany(orders)
+
+        return {sucess: true, processed: orders.length };
     }
 )
